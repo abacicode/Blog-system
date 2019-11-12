cf74d6ef42ce81f8de5e66d169664c9c822f8ac4
Chun Loong <chun_loong@outlook.com>
2019-11-12 23:01:30 +0100

    Initial Commit

---
+++ admin/index-admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Log In</title>
+  <link rel="stylesheet" type="text/css" href="index-admin.css">
+</head>
+<body>
+  <form class="log-in" action="index.php" method="post">
+    <div class="input">Username : <input type="text" name="user-name"></input></div>
+    <div class="input">Password : <input type="text" name="password"></input></div>
+    <div class="input" id="submitDiv"><button id= "submitButton" type="submit" name="submit"></button></div>
+  </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Blog</title>
+  <link rel="stylesheet" href="index.css">
+</head>
+<body>
+  <nav id="navBar">
+    <a class="navOption" href="index.php">Home</a>
+    <a class="navOption" href="admin/index-admin.php">Admin</a>
+  </nav>
+  <section id= "title">
+    <div class="titleContent" id="postNumber">-Post 1-</div>
+    <div class="titleContent">A journey to freedom</div>
+  </section>
+</body>
+</html>
