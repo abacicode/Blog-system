7ce6fc6e79b0f6ac8f0aa48906bae85fd07c4bb4
Chun Loong <chun_loong@outlook.com>
2019-11-13 21:06:49 +0100

    Added npm, gulp. Coded gulpfile.js for SCSS compiler.

--- index.php
+++ index.php
@@ -5,16 +5,27 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Blog</title>
-  <link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
+  
   <nav id="navBar">
     <a class="navOption" href="index.php">Home</a>
     <a class="navOption" href="admin/index-admin.php">Admin</a>
   </nav>
+
   <section id= "title">
     <div class="titleContent" id="postNumber">-Post 1-</div>
-    <div class="titleContent">A journey to freedom</div>
+    <div class="titleContent">A journey</div>
+  </section>
+
+  <section id= "picture">
+    <div class="titleContent" id="postNumber">
+      <img src= "images/journey1.jpg" alt="Jinn Making Shoes">
+    </div>
+  </section>
+
+  <section id="image">
   </section>
 </body>
 </html>
