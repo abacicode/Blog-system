3942f6d312db1f7c837e080cabdf339aae433f3b
Chun Loong <chun_loong@outlook.com>
2019-11-14 20:33:27 +0100

    Added PHP connection system, Generated a table in SQL.

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-  return gulp.src('index.scss').pipe(sass()).pipe(gulp.dest('css'))
+  return gulp.src('index.scss','admin/admin-login.scss').pipe(sass()).pipe(gulp.dest('css'))
 })
+
+
+/*
+gulp.task('adminsass',function(){
+  return gulp.src('')
+})
+*/
--- index.php
+++ index.php
@@ -1,3 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "Jinn";
+$password = "R1465my5187J";
+$name= "Blog-System";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$name);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
+
+<?php
+  $query = "SELECT * FROM blog";
+  $select_all_categories_query = mysqli_query($conn, $query);
+  while ($row = mysqli_fetch_assoc($select_all_categories_query)){
+      $post_number= $row['number'];
+      $post_date= $row['date'];
+      $post_title= $row['title'];
+      $post_image= $row['image'];
+      $post_description= $row['description'];
+  }
+  echo $post_description;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,24 +36,36 @@
   <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-  
+
   <nav id="navBar">
     <a class="navOption" href="index.php">Home</a>
-    <a class="navOption" href="admin/index-admin.php">Admin</a>
+    <a class="navOption" href="admin/admin-login.php">Admin</a>
   </nav>
 
-  <section id= "title">
+  <section id= "info">
     <div class="titleContent" id="postNumber">-Post 1-</div>
-    <div class="titleContent">A journey</div>
+    <div class="titleContent" id="date"> November 14, 2019</div>
+    <div class="titleContent" id="title">A journey</div>
   </section>
 
   <section id= "picture">
-    <div class="titleContent" id="postNumber">
+    <div>
       <img src= "images/journey1.jpg" alt="Jinn Making Shoes">
     </div>
   </section>
 
-  <section id="image">
+  <section id="description">
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna
+      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit
+      anim id est laborum.
+    </div>
   </section>
+
 </body>
 </html>
